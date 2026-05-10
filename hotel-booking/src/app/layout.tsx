949d16b20eb273c05f97651da32682e688f96730
alex gomes <alexgomesoffice@gmail.com>
2026-05-10 16:43:41 +0600

    sun moon flicker isssue been fixed

--- hotel-booking/src/app/layout.tsx
+++ hotel-booking/src/app/layout.tsx
@@ -6,16 +6,38 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              try {
+                var theme = localStorage.getItem('theme');
+                var supportDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches === true;
+                if (!theme && supportDarkMode) theme = 'dark';
+                if (!theme && !supportDarkMode) theme = 'light';
+                
+                if (theme === 'light') {
+                  document.documentElement.classList.add('light');
+                } else {
+                  document.documentElement.classList.add('dark');
+                }
+              } catch (e) {}
+
+            `,
+          }}
+        />
+      </head>
       <body>
         {children}
         <Toaster
-          position="top-right"
+          position="bottom-right"
           theme="dark"
           richColors
           closeButton
         />
       </body>
     </html>
+
   )
 }
--- hotel-booking/src/hooks/use-theme.tsx
+++ hotel-booking/src/hooks/use-theme.tsx
@@ -5,26 +5,30 @@ import { useEffect, useState } from 'react'
 type Theme = 'light' | 'dark'
 
 export function useTheme() {
-  const [theme, setTheme] = useState<Theme>(() => {
-    if (typeof window === 'undefined') return 'dark'
-    return (localStorage.getItem('theme') as Theme) || 'dark'
-  })
+  const [theme, setTheme] = useState<Theme>('dark')
+  const [isInitialized, setIsInitialized] = useState(false)
 
   useEffect(() => {
+    const savedTheme = localStorage.getItem('theme') as Theme
+    if (savedTheme) {
+      setTheme(savedTheme)
+    }
+    setIsInitialized(true)
+  }, [])
+
+  useEffect(() => {
+    if (!isInitialized) return
+
     const root = document.documentElement
-
-    // remove both first (important)
     root.classList.remove('light', 'dark')
-
-    // add current theme
     root.classList.add(theme)
-
     localStorage.setItem('theme', theme)
-  }, [theme])
+  }, [theme, isInitialized])
+
 
   const toggleTheme = () => {
     setTheme(prev => (prev === 'dark' ? 'light' : 'dark'))
   }
 
-  return { theme, toggleTheme }
+  return { theme, toggleTheme, isInitialized }
 }
